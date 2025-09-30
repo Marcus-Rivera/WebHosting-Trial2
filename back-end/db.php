239650a31461389446e109393a8fb6b6464effdc
Kayle Matthew Calagui <kmmcalagui@mymail.mapua.edu.ph>
2025-09-30 09:23:01 +0800

    test connections

---
+++ back-end/db.php
@@ -0,0 +1,24 @@
+<?php
+// db.php
+$host = "localhost";     // database host (XAMPP/MAMP usually localhost)
+$user = "root";          // MySQL username (default is root in XAMPP)
+$pass = "";              // MySQL password (default empty in XAMPP)
+$dbname = "tratrabaho"; // your database name
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die(json_encode([
+        "status" => "error",
+        "message" => "Connection failed: " . $conn->connect_error
+    ]));
+}
+
+// If you just want to test connection:
+echo json_encode([
+    "status" => "success",
+    "message" => "Database connected successfully"
+]);
+?>
